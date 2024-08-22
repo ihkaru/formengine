f59399f07412ec0aad8638042320401bb4188b59
ihkaru <ihza2karunia@gmail.com>
2024-08-22 16:51:29 +0700

    init backend

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('google_id')->unique();
             $table->string('jenis')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
