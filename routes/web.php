4bc1a4e4c50529014e78d7c7a0c621a9d8ab80a9
ihkaru <ihza2karunia@gmail.com>
2024-08-24 17:05:59 +0700

    add artisan controller

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Filament\Pages\RespondentSurvey;
-
+use App\Http\Controllers\ArtisanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("/run",[ArtisanController::class,"run"]);
 
 // routes/web.php
 
