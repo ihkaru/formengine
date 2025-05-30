0602fa6b8b9c93dfa92fda9c6e84975928a9578d
ihkaru <ihza2karunia@gmail.com>
2025-05-30 11:38:42 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
 Route::get('auth/google', [GoogleAuthController::class, 'redirectToGoogle']);
