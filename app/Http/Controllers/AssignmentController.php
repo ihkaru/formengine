c50abe22edf6c2256ec7a729b7841ddd8145ef4c
ihkaru <ihza2karunia@gmail.com>
2025-06-01 21:31:45 +0700

    test

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -47,7 +47,6 @@ class AssignmentController extends Controller
     public function sync(Request $request, Assignment $assignment)
     {
         $user = auth()->user();
-
         $kegiatanId = $assignment->kegiatan_id;
         // Validasi data JSON 'answers' dan field lain
         $validatedJsonData = $request->validate([
