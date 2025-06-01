112d667fc37992535f799295f5ce5d53a0c21369
ihkaru <ihza2karunia@gmail.com>
2025-06-02 04:04:55 +0700

    tettst

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,18 @@ use App\Http\Controllers\CantikController;
 Route::get('/', function () {
     return view('welcome');
 })->name("home");
+Route::get('/storage/kegiatan_uploads/{filename}', function ($filename) {
+    $path = storage_path('app/public/kegiatan_uploads/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path, [
+        'Content-Type' => 'image/webp',
+        'Access-Control-Allow-Origin' => '*', // Or your domain
+    ]);
+});
 
 Route::get('/desacantik/kelurahanpulaupedalaman', [CantikController::class, "kelPulauPedalaman"])->name("cantik.pedalaman");
 
