693b5a35aaa48bc86b0d98888e7e68435e3ecf73
ihkaru <ihza2karunia@gmail.com>
2025-07-22 11:16:49 +0700

    fixing route

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,10 @@ Route::get('/uploads/{path}', function ($path) {
 })->where('path', '.*');
 
 
-Route::get('/desa-cantik', [CantikController::class, "index"])->name("cantik.index");
-Route::get('/desacantik/kelurahanpulaupedalaman', [CantikController::class, "kelPulauPedalaman"])->name("cantik.pedalaman");
-Route::get('/desacantik/desawajokhilir', [CantikController::class, "desWajokHilir"])->name("cantik.wajokhilir");
-Route::get('/desacantik/desasejegi', [CantikController::class, "desSejegi"])->name("cantik.sejegi");
+Route::get('/desacantik', [CantikController::class, "index"])->name("cantik.index");
+Route::get('/desa-cantik/kelurahanpulaupedalaman', [CantikController::class, "kelPulauPedalaman"])->name("cantik.pedalaman");
+Route::get('/desa-cantik/desawajokhilir', [CantikController::class, "desWajokHilir"])->name("cantik.wajokhilir");
+Route::get('/desa-cantik/desasejegi', [CantikController::class, "desSejegi"])->name("cantik.sejegi");
 Route::get('/login', function () {
     auth()->logout();
     return redirect()->to('/admin/login');
